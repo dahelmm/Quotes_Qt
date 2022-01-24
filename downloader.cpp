ee3b3d864daf57e937337be5d3f96db55142a46b
dahelmm <dahelmm@mail.ru>
2022-01-24 09:54:50 +0300

    .

--- downloader.cpp
+++ downloader.cpp
@@ -2,5 +2,26 @@
 
 Downloader::Downloader(QObject *parent):QObject(parent)
 {
+    man = new QNetworkAccessManager(this);
+    connect(man,SIGNAL(finished(QNetworkReply*)),this,SLOT(slotFinished(QNetworkReply*)));
+}
 
+void Downloader::download(const QUrl &url)
+{
+    QNetworkRequest req(url);
+    QNetworkReply *rep = man->get(req);
+    connect(rep,SIGNAL(downloadProgress(quint64,quint64)),this,SIGNAL(downloadProgress(quint64,quint64)));
+}
+
+void Downloader::slotFinished(QNetworkReply *reply)
+{
+    if(reply->error() != QNetworkReply::NoError)
+    {
+        emit error();
+    }
+    else
+    {
+        emit done(reply->url(),reply->readAll());
+    }
+    reply->deleteLater();
 }
